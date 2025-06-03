39dff611acbcb041075350b4ab1b3038e10c2d60
simwawatim <simwawatimothy64@gmail.com>
2025-06-03 12:03:32 +0200

    added login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,9 +1,9 @@
+import LoginForm from "../../../componets/login/login";
+
 export default function Login() {
   return (
-    <div>
-      <h1>Login Page</h1>
-      <p>Please enter your credentials to log in.</p>
-      {/* Add your login form here */}
-    </div>
+    <>
+        <LoginForm />
+    </>
   );
 }
