47c47c83752df1ad689383d89ab1a11b61ecf14d
simwawatim <simwawatimothy64@gmail.com>
2025-06-03 11:51:02 +0200

    intail

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,9 @@
+export default function Login() {
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <p>Please enter your credentials to log in.</p>
+      {/* Add your login form here */}
+    </div>
+  );
+}
