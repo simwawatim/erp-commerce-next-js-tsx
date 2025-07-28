01dd54043ce08fbf5a9a354c243e30c388f8967c
simwawatim <simwawatimothy64@gmail.com>
2025-07-28 09:16:33 +0200

    update the base url for chat api

--- componets/customer/navbar.tsx
+++ componets/customer/navbar.tsx
@@ -64,7 +64,7 @@ const CustomerDashboard = () => {
     setIsThinking(true);
 
     try {
-      const res = await fetch("http://127.0.0.1:8000/api/chat/", {
+      const res = await fetch("https://uat.pythonanywhere.com/api/chat/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
